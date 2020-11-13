5a62dd0fd7f0669bfe1721919c5eff1a7c7baed7
gunlee1226 <74275285+gunlee1226@users.noreply.github.com>
2020-11-13 14:04:05 +0900

    오전수업/오버로드

---
+++ helloWorld/helloworld/src/day4/CConverterApp.java
@@ -0,0 +1,24 @@
+package day4;
+
+
+public class CConverterApp {
+
+    public static void main(String[] args) {
+
+        double dollar;
+        double won;
+        
+        CConverter.setRate(1118.70);
+        
+        CConverter.toDoller(1000000);
+        System.out.println("백만원은" + CConverter.toDoller(1000000));
+        
+        //백만원을 달러로 출력
+
+        
+        //100달려를 원으로 출력
+        
+        
+    }
+
+}
---
+++ helloWorld/helloworld/src/day4/MyMathApp.java
@@ -0,0 +1,15 @@
+package day4;
+
+
+public class MyMathApp {
+
+	public static void main(String[] args) {
+
+		MyMath math = new MyMath();
+		
+        System.out.println(math.CircleArea(30));
+        
+        
+    }
+
+}
