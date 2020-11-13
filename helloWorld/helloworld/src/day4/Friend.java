820d338e3766dd2c5bd1ed2f5c5c80bb1127ec6f
gunlee1226 <74275285+gunlee1226@users.noreply.github.com>
2020-11-13 17:42:25 +0900

    클래스 배열.

--- helloWorld/helloworld/src/day4/Friend.java
+++ helloWorld/helloworld/src/day4/Friend.java
@@ -1,13 +1,11 @@
 package day4;
 
-
-
 public class Friend {
-    private String name;
-    private String hp;
-    private String school;
-	
-    //필요한 메소드 작성
+	private String name;
+	private String hp;
+	private String school;
+
+	// 필요한 메소드 작성
 
 	public String getName() {
 		return name;
@@ -33,18 +31,15 @@ public class Friend {
 		this.school = school;
 	}
 
-	public Friend(String name, String hp, String school) {
-		super();
-		this.name = name;
-		this.hp = hp;
-		this.school = school;
-	}
-	String inname = " ";   
-	
-    public void showInfo(){
-        System.out.println("이름:"+name+"  핸드폰:"+hp+"  학교:"+school);
-  
-    }
+	public Friend() {
 
+	}
+
+	public Friend(String n, String hpp, String sch) {
+		super();
+		this.name = n;
+		this.hp = hpp;
+		this.school = sch;
+	}
 
 }
--- helloWorld/helloworld/src/day4/FriendApp.java
+++ helloWorld/helloworld/src/day4/FriendApp.java
@@ -1,24 +1,47 @@
 package day4;
+
 import java.util.Scanner;
 
-
 public class FriendApp {
-    public static void main(String[] args) {
-        Friend[] friendArray = new Friend[3];
-        
-        Scanner sc = new Scanner(System.in);
-        System.out.println("친구를 3명 등록해 주세요");
-        
-        
-            // 친구정보 입력받기
-            // 입력받은 친구정보를 공백으로 분리
-            // Friend 객체 생성하여 데이터 넣기
-            // 배열에 추가하기
-        // 친구정보 출력
-        for (int i = 0; i < friendArray.length; i++) {
-            
-        	friendArray[i] = ();//친구정보 출력 메소드 호출
-        }
-        sc.close();
-    }
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		Friend[] friendArray = new Friend[3];
+
+		System.out.println("친구를 3명 등록해 주세요");
+		String input1 = sc.nextLine();
+		String input2 = sc.nextLine();
+		String input3 = sc.nextLine();
+
+		String[] s1 = new String[3];
+		s1[0] = input1;
+		s1[1] = input2;
+		s1[2] = input3;
+
+		Friend F = new Friend(input1, input2, input3);
+		input1.split(" ");
+		input2.split(" ");
+		input3.split(" ");
+
+//    	F.setHp(s1[0]);
+//    	F.setName(s1[1]);
+//    	F.setSchool(s1[2]);
+//        System.out.println(F.getHp());
+//        System.out.println(F.getName());
+//        System.out.println(F.getSchool());
+
+		for (int i = 0; i < friendArray.length; i++) {
+			friendArray[i] = F;
+		}
+		// 친구정보 입력받기
+		// 입력받은 친구정보를 공백으로 분리
+		// Friend 객체 생성하여 데이터 넣기
+		// 배열에 추가하기
+		// 친구정보 출력
+		for (int i = 0; i < friendArray.length; i++) {
+			System.out.println(friendArray[i] = F);
+		}
+
+	}
 }
