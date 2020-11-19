cc8d99bfae896375f7712a9612b523bfeec17e73
gunlee1226 <74275285+gunlee1226@users.noreply.github.com>
2020-11-19 18:52:48 +0900

    날짜조회하기 만들기.

---
+++ helloWorld/helloworld/src/day8/AccountDAO.java
@@ -0,0 +1,234 @@
+package day8;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountDAO {
+
+	private Connection getConnection() throws SQLException {
+		Connection conn = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String dburl = "jdbc:oracle:thin:@localhost:1521:orcl";
+			conn = DriverManager.getConnection(dburl, "gunlee", "0000");
+		} catch (ClassNotFoundException e) {
+			System.err.println("JDBC 드라이버 로드 실패!");
+		}
+		return conn;
+	}
+
+	public void depositAccount(AccountVO vo) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			conn = getConnection();
+
+			String query = " INSERT INTO ACCOUNT(account_id,deposit, withdraw,tr_date,balance)\r\n"
+					+ "select id_seq.NEXTval, ? , 0 ,SYSDATE, balance+? from account\r\n"
+					+ "where account_id  = (select max(account_id) from account)";
+			pstmt = conn.prepareStatement(query);
+
+			pstmt.setInt(1, vo.getDeposit());
+			pstmt.setInt(2, vo.getDeposit());
+			
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error:" + e);
+			}
+		}
+
+	}
+
+	public void withdrawAccount(AccountVO vo) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+
+			conn = getConnection();
+
+			String query =  " INSERT INTO ACCOUNT(account_id,deposit, withdraw,tr_date,balance)\r\n"
+					+ "select id_seq.NEXTval, 0 , ? ,SYSDATE, balance-? from account\r\n"
+					+ "where account_id  = (select max(account_id) from account)";
+			pstmt = conn.prepareStatement(query);
+
+			pstmt.setInt(1, vo.getWithdraw());
+			pstmt.setInt(2, vo.getWithdraw());
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error:" + e);
+			}
+		}
+	}
+
+	
+	
+	public List<AccountVO> getListAll() {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		List<AccountVO> AList = new ArrayList<>();
+
+		try {
+			conn = getConnection();
+
+			String query = "select account_id, deposit, withdraw, tr_date, balance from account order by account_id desc";
+			pstmt = conn.prepareStatement(query);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				AccountVO vo = new AccountVO();
+				int account_id = rs.getInt(1);
+				int deposit = rs.getInt(2);
+				int withdraw = rs.getInt(3);
+				String tr_date = rs.getString(4);
+				int balance = rs.getInt(5);
+				
+				vo.setAccount_id(account_id);
+				vo.setDeposit(deposit);
+				vo.setWithdraw(withdraw);
+				vo.setTr_date(tr_date);
+				vo.setBalance(balance);
+				
+				AList.add(vo);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error:" + e);
+			}
+		}
+		
+		return AList;
+	}
+
+	public int getBalance(){
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int current = 0;
+		
+		try {
+			conn = getConnection();
+
+			String query = "select balance from account\r\n"
+					+ "where account_id = (select max(account_id) from account)";
+			pstmt = conn.prepareStatement(query);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				AccountVO vo = new AccountVO();
+				current = rs.getInt(1);
+								
+				}
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error:" + e);
+			}
+		}
+		
+		return current;
+	}
+	
+	public String fordate(){
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String adate = "null";
+		
+		try {
+			conn = getConnection();
+
+			String query = "SELECT \r\n"
+					+ "*\r\n"
+					+ "FROM  account\r\n"
+					+ "WHERE tr_date > To_date('?') and  tr_date<= to_date('?')";
+			pstmt = conn.prepareStatement(query);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				AccountVO vo = new AccountVO();
+				adate = rs.getString(1);
+				adate = rs.getString(2);
+								
+				}
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error:" + e);
+			}
+		}
+		
+		return adate;
+	}
+}
---
+++ helloWorld/helloworld/src/day8/AccountMain.java
@@ -0,0 +1,81 @@
+package day8;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class AccountMain {
+
+	public static List<AccountVO> AList;
+	public static AccountDAO dao = new AccountDAO();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int input;
+		
+		boolean tf = true;
+		
+		AccountVO vo = new AccountVO();
+		while (tf) {
+
+			System.out.println("");
+			System.out.println("---------------------------------------");
+			System.out.println("1.예금 | 2.출금 | 3.잔고 |4.날짜조회 | 5.종료");
+			System.out.println("---------------------------------------");
+			System.out.print("선택>");
+			int sel = sc.nextInt();
+
+			switch (sel) {
+			case 1:
+				System.out.print("예금액>");
+				input = sc.nextInt();
+				vo.setDeposit(input);
+				dao.depositAccount(vo);
+				printAccount();
+				break;
+			case 2:
+				System.out.print("출금액>");
+				input = sc.nextInt();
+				vo.setWithdraw(input);
+				dao.withdrawAccount(vo);
+				printAccount();
+				break;
+			case 3:
+				System.out.print("잔고액>");
+			System.out.println(dao.getBalance());	
+				break;
+			case 4:
+				System.out.println("기간조회 입력 두개");
+				input = sc.nextInt();
+				input = sc.nextInt();
+				System.out.println(dao.fordate());
+				break;
+			case 5:
+				System.out.println("*****프로그램을 종료합니다.*****");
+				tf = false;
+				break;
+
+			default :
+                System.out.println("다시입력해주세요");
+                break;
+			}
+				
+		}
+
+	}
+
+	
+
+	public static void printAccount() {
+		System.out.println("*****통장 거래내역 출력 (시작)  *****");
+
+		// 출력코드작성
+		List<AccountVO> AList = dao.getListAll();
+		for (AccountVO mvo : AList) {
+			System.out.println(mvo);
+		}
+
+		System.out.println("*****통장 거래내역 출력 (끝)  *****");
+	}
+	
+
+}
