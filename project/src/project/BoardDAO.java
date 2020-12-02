31771de370a67d8ce04aa905061d42741f970a01
gunlee1226 <74275285+gunlee1226@users.noreply.github.com>
2020-12-02 15:52:27 +0900

    새로연동확인

---
+++ project/src/project/BoardDAO.java
@@ -0,0 +1,149 @@
+package project;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import sun.awt.CharsetString;
+
+public class BoardDAO {
+
+   public int insert(BoardVO vo) {
+      Connection conn = null;
+      PreparedStatement pstmt = null;
+      int count = 0 ;
+      
+      try {
+         Class.forName("oracle.jdbc.driver.OracleDriver");
+         
+         String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+         conn = DriverManager.getConnection(url, "gunlee", "0000");
+         
+         String query ="insert into board values (B_NO_SEQ.nextval,?,sysdate,0,0,?,?,20004)";
+         pstmt = conn.prepareStatement(query);   
+         
+         pstmt.setString(1, vo.getB_title());
+         pstmt.setString(2, vo.getB_contents());
+         pstmt.setString(3, vo.getB_comment());
+         
+      
+         count = pstmt.executeUpdate();
+         
+         System.out.println(count + "건 등록");
+         
+      } catch (ClassNotFoundException e) {
+         System.out.println("error: 드라이버 로딩 실패 - " + e);
+      } catch (SQLException e) {
+         System.out.println("error:" + e);
+      } finally {
+         try {
+            if (pstmt != null) pstmt.close();
+            if (conn != null) conn.close();
+         } catch (SQLException e) {
+            System.out.println("error:" + e);
+         }
+      }
+      return count;
+   }
+   
+   /*
+   public void delete(String no) {
+      Connection conn = null;
+      PreparedStatement pstmt = null;
+      int count = 0 ;
+      
+      try {
+         Class.forName("oracle.jdbc.driver.OracleDriver");
+         
+         String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+         conn = DriverManager.getConnection(url, "gunlee", "0000");
+         
+         String query ="delete from Board where b_no =?";
+         pstmt = conn.prepareStatement(query);   
+         pstmt.setString(1, no);
+            
+
+         count = pstmt.executeUpdate();
+         
+         System.out.println(count + "건 삭제");
+         
+      } catch (ClassNotFoundException e) {
+         System.out.println("error: 드라이버 로딩 실패 - " + e);
+      } catch (SQLException e) {
+         System.out.println("error:" + e);
+      } finally {
+         try {
+            if (pstmt != null) pstmt.close();
+            if (conn != null) conn.close();
+         } catch (SQLException e) {
+            System.out.println("error:" + e);
+         }
+      }
+   
+   }
+   
+   */
+   public List<BoardVO> getList() {
+
+      // 0. import java.sql.*;
+      Connection conn = null;
+      PreparedStatement pstmt = null;
+      ResultSet rs = null;
+      List<BoardVO> list = new ArrayList<BoardVO>();
+
+      try {
+         // 1. JDBC 드라이버 (Oracle) 로딩
+         Class.forName("oracle.jdbc.driver.OracleDriver");
+         
+         // 2. Connection 얻어오기
+         String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+         conn = DriverManager.getConnection(url, "gunlee", "0000");
+         
+         // 3. SQL문 준비 / 바인딩 / 실행
+         String query = "select b_no, b_date, MEM_CODE,b_title, b_view, b_commview from board order by b_no asc";
+         pstmt = conn.prepareStatement(query);   
+         rs = pstmt.executeQuery();
+         // 4.결과처리
+         while(rs.next()) {
+              
+        	int b_no = rs.getInt("b_no");
+            String b_title = rs.getString("b_title");
+            String b_date = rs.getString("b_date");
+            int b_view = rs.getInt("b_view");
+            int b_commview = rs.getInt("b_commview");
+            String MEM_CODE = rs.getString("MEM_CODE");
+
+         
+            BoardVO vo = new BoardVO(b_no, b_title, b_date, b_view, b_commview, MEM_CODE);
+          
+            list.add(vo);
+           
+         }
+      } catch (ClassNotFoundException e) {
+         System.out.println("error: 드라이버 로딩 실패 - " + e);
+      } catch (SQLException e) {
+         System.out.println("error:" + e);
+      } finally {
+         // 5. 자원정리
+         try {
+            if (pstmt != null) {
+               pstmt.close();
+            }
+            if (conn != null) {
+               conn.close();
+            }
+         } catch (SQLException e) {
+            System.out.println("error:" + e);
+         }
+
+      }
+
+      return list;
+      
+   }
+
+}
---
+++ project/src/project/BoardVO.java
@@ -0,0 +1,127 @@
+package project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoardVO {
+		   
+   private int b_no;
+   private String b_title;
+   private String b_date;
+   private String b_contents;
+   private int b_view;
+   private int b_commview;
+   private String b_comment;
+   private String MEM_CODE;
+   
+
+
+   
+   public BoardVO(int b_no, String b_title, String b_date, int b_view, int b_commview, String MEM_CODE) {
+      super();
+      this.b_no = b_no;
+      this.b_title = b_title;
+      this.b_date = b_date;
+      this.b_view = b_view;
+      this.b_commview = b_commview;
+      this.MEM_CODE = MEM_CODE;
+   }
+   
+
+public String getMEM_CODE() {
+	return MEM_CODE;
+}
+	public void setMEM_CODE(String MEM_CODE) {
+		this.MEM_CODE = MEM_CODE;
+	}
+
+
+public int getB_no() {
+      return b_no;
+   }
+
+
+
+   public void setB_no(int b_no) {
+      this.b_no = b_no;
+   }
+
+
+
+   public String getB_title() {
+      return b_title;
+   }
+
+
+
+   public void setB_title(String b_title) {
+      this.b_title = b_title;
+   }
+
+
+
+   public String getB_date() {
+      return b_date;
+   }
+
+
+
+   public void setB_date(String b_date) {
+      this.b_date = b_date;
+   }
+
+
+
+   public String getB_contents() {
+      return b_contents;
+   }
+
+
+
+   public void setB_contents(String b_contents) {
+      this.b_contents = b_contents;
+   }
+
+
+
+   public int getB_view() {
+      return b_view;
+   }
+
+
+
+   public void setB_view(int b_view) {
+      this.b_view = b_view;
+   }
+
+
+
+   public int getB_commview() {
+      return b_commview;
+   }
+
+
+
+   public void setB_commview(int b_commview) {
+      this.b_commview = b_commview;
+   }
+
+
+
+   public String getB_comment() {
+      return b_comment;
+   }
+
+
+
+   public void setB_comment(String b_comment) {
+      this.b_comment = b_comment;
+   }
+
+
+
+   @Override
+   public String toString() {
+      return "BoardVO "+"b_no"+"b_title" + "b_date"+"b_contents"+"b_view"+"b_commview"+"b_comment" + "\n";
+   }
+}
